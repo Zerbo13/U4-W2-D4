20e5d6559bd8f94a6909defe64fbcfd5e2ffa323
Mattia Zerbini <zermatty6@gmail.com>
2026-01-15 16:55:00 +0100

    exercise 1

--- U4-W2-D4/src/main/java/Mattiazerbini/Application.java
+++ U4-W2-D4/src/main/java/Mattiazerbini/Application.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import Mattiazerbini.entities.Customer;
 import Mattiazerbini.entities.Order;
@@ -33,7 +34,17 @@ public class Application {
             Order o1 = new Order(10L, "In arrivo", LocalDate.of(2025, 1, 10), LocalDate.of(2025, 1, 15), List.of(p2, p5), c1);
             Order o2 = new Order(11L, "Imballaggio", LocalDate.of(2025, 1, 14), LocalDate.of(2025, 1, 21), List.of(p3), c2);
             Order o3 = new Order(12L, "Consegnato al corriere", LocalDate.of(2025, 1, 12), LocalDate.of(2025, 1, 18), List.of(p4, p1, p6), c3);
-        Order o4 = new Order(13L, "In consegna", LocalDate.of(2025, 1, 13), LocalDate.of(2025, 1, 19), List.of(p1, p3, p6), c1);
+            Order o4 = new Order(13L, "In consegna", LocalDate.of(2025, 1, 13), LocalDate.of(2025, 1, 19), List.of(p1, p3, p6), c1);
 
+            List<Order> orders = List.of(o1, o2, o3, o4);
+
+
+
+        System.out.println("ESERCIZIO 1: ");
+        Map<Customer, List<Order>> ordiniPerCliente =
+                orders.stream()
+                .collect(Collectors.groupingBy(Order::getCustomer));
+                ordiniPerCliente.forEach((customer, customerOrders)
+                -> System.out.println(customer.getName() + " ha effettuato " + customerOrders.size() + " ordini"));
     }
 }
